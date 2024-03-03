e811e770dcf52b88c02ddf1466854f05cfdabd15
MirelaVasileva <vasilevamirela63@gmail.com>
2024-03-03 17:22:36 +0200

    fix: styling changes

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -5,11 +5,13 @@ export const Hero = () => {
     return (
         <div id='hero-div'>
             <div className='hero-img-div'>
-                <section className='hero-section'>
-                <h1 className='title'>Don't know what to cook?</h1>
-                <h2 className='description'>Tell us what you have in your fridge and we will suggest some recipies</h2>
-                <button id='hero-button'>Get started</button>
-                </section>
+                <div className='hero-inner-div'>
+                    <section className='hero-section'>
+                        <h1 className='title'>Don't know what to cook?</h1>
+                        <h2 className='description'>Tell us what you have in your fridge and we will suggest some recipies</h2>
+                        <button id='hero-button'>Get started</button>
+                    </section>
+                </div>
             </div>
         </div>
     )
