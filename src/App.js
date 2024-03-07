4653c817f460d99c5070f6c48ae4222f61c64b91
MirelaVasileva <vasilevamirela63@gmail.com>
2024-03-07 23:37:11 +0200

    feat: added react-router
    added react router to prepare for future use (recipe full page)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Hero } from './Components/Hero/Hero';
 import { Navbar } from './Components/Navbar/Navbar';
 import { RecipeMain } from './Components/Recipes/RecipeMain/RecipeMain.jsx';
 import { RecipeProvider } from './RecipeContext.js';
 
+
 function App() {
   return (
     <RecipeProvider>
       <div className="App">
         <Navbar/>
-        <Hero />
-        <RecipeMain />
+    <Routes>
+        <Route path='/'  element={<MainPage/>} />
+    </Routes>
+        
       </div>
     </RecipeProvider>
   );
+}
+const MainPage = () =>{
+  return (
+    <>
+<Hero />
+<RecipeMain />
+    </>
+  )
 }
 
 export default App;
